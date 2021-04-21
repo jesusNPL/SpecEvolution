8b5eebaeb786fea9e7e44b4ce88b2c56cee9f521
jesusnpl <jesuspintoledezma@gmail.com>
2021-04-21 17:32:45 -0500

    Add readspectra

---
+++ R/readSPEC.R
@@ -0,0 +1,47 @@
+
+
+demon_readSPEC <- function(path, metadata, state, format) { 
+  if ( ! ("spectrolab" %in% installed.packages())) {install.packages("spectrolab", dependencies = T)} 
+  if ( ! ("dplyr" %in% installed.packages())) {install.packages("dplyr", dependencies = T)}
+  if ( ! ("tidyr" %in% installed.packages())) {install.packages("tidyr", dependencies = T)}
+  
+  # Load required packages
+  require(spectrolab)
+  require(dplyr)
+  require(tidyr)
+  
+  # Filter information form the metadata to be joined with the spectra data
+  meta_state <- metadata %>% 
+    filter(COUNTRY_STATE_CODE == state) %>% 
+    select(SPECIES, DATE, YEAR, LEAF.SPEC.ID, COUNTRY_STATE, SPCODE, fulcrum_id, LEAF_REPLICATE)
+  
+  # Read spectra data
+  spec <- read_spectra(
+    path = ruta, 
+    format = format, 
+    recursive = TRUE, 
+    exclude_if_matches = c("BAD", "WR"))
+  
+  # Transform spectra to dataframe
+  tm <- as.data.frame(spec, fix_names = "none", metadata = TRUE)
+  # Join metadata with spectra
+  spec_data <- full_join(meta_state, tm, by = c("LEAF.SPEC.ID" = "sample_name"))
+  # Transform to spectra format
+  spect <- as_spectra(spec_data, name_idx = 1, meta_idxs = c(2:8))
+  # Transform to dataframe format
+  spect_df <- as.data.frame(spect, fix_names = "none", metadata = TRUE)
+  # Save both objects 
+  obj <- list(spectra = spect, spectra_df = spect_df)
+  
+  return(obj)
+}
+
+#state <- "OH"
+#ruta = "*Ohio/" 
+#metaDT <- read.csv("2019.META.DATA/2019.SVC.DATA.LABELS.csv")
+
+#bbb <- demon_readSPEC(path = ruta, metadata = metaDT, state = state, format = "sig")
+
+#bbb[[1]]
+
+#bbb[[2]][1:10, 1:10]
